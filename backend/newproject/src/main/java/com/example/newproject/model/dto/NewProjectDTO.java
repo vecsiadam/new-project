4f5f01c25cd43fc0c0528268316a76c6fc553b67
Adam Vecsi <adamvecsi94@gmail.com>
2019-11-06 22:49:42 +0100

    fix: delete comments

--- backend/newproject/src/main/java/com/example/newproject/model/dto/NewProjectDTO.java
+++ backend/newproject/src/main/java/com/example/newproject/model/dto/NewProjectDTO.java
@@ -14,47 +14,4 @@ public class NewProjectDTO {
 	private String description;
 	private int ingridientsNumber;
 	
-//	public NewProjectDTO() {
-//	}
-//	
-//	public NewProjectDTO(String name, Set<String> ingredients, String description, int ingridientsNumber) {
-//		super();
-//		this.name = name;
-//		this.ingredients = ingredients;
-//		this.description = description;
-//		this.ingridientsNumber = ingridientsNumber;
-//	}
-//	
-//	public String getName() {
-//		return name;
-//	}
-//	
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	
-//	public Set<String> getIngredients() {
-//		return ingredients;
-//	}
-//	
-//	public void setIngredients(Set<String> ingredients) {
-//		this.ingredients = ingredients;
-//	}
-//	
-//	public String getDescription() {
-//		return description;
-//	}
-//	
-//	public void setDescription(String description) {
-//		this.description = description;
-//	}
-//	
-//	public int getIngridientsNumber() {
-//		return ingridientsNumber;
-//	}
-//	
-//	public void setIngridientsNumber(int ingridientsNumber) {
-//		this.ingridientsNumber = ingridientsNumber;
-//	}
-	
 }
