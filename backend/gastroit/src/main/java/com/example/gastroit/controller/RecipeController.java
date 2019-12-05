dd131e679b487febcc7c873f93549ca0760b0fc7
Adam Vecsi <adamvecsi94@gmail.com>
2019-12-05 09:00:45 +0100

    fix: recipe controller fix

--- backend/gastroit/src/main/java/com/example/gastroit/controller/RecipeController.java
+++ backend/gastroit/src/main/java/com/example/gastroit/controller/RecipeController.java
@@ -2,7 +2,6 @@ package com.example.gastroit.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,33 +15,32 @@ import com.example.gastroit.model.dto.RecipeDTO;
 import com.example.gastroit.model.entity.Recipe;
 import com.example.gastroit.service.RecipeService;
 
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/recipe")
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class RecipeController {
 
-	@Autowired
-	private RecipeService repositoryService;
+	private final RecipeService recipeService;
 
 	@PostMapping(consumes = "application/json", produces = "application/json")
 	public Recipe create(@RequestBody RecipeDTO recipe) {
-		return repositoryService.create(recipe);
+		return recipeService.create(recipe);
 	}
 
 	@GetMapping(produces = "application/json")
 	public List<Recipe> list() {
-		return repositoryService.list();
+		return recipeService.list();
 	}
 
 	@PutMapping(path = "/{recipeId}", consumes = "application/json", produces = "application/json")
 	public Recipe update(@PathVariable("recipeId") Long recipeId, @RequestBody RecipeDTO recipe) {
-		return repositoryService.update(recipeId, recipe);
+		return recipeService.update(recipeId, recipe);
 	}
 
 	@DeleteMapping(path = "/{recipeId}")
 	public void delete(@PathVariable("recipeId") Long recipeId) {
-		repositoryService.delete(recipeId);
+		recipeService.delete(recipeId);
 	}
 }
